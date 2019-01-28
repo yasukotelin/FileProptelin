cc11d9e32755e4e6c690d8565274ca2389da14b3
yasuhiro.kato <yasuhiro.kato@cct-inc.co.jp>
2019-01-28 17:58:51 +0900

    add: プロジェクトの追加

--- FileProptelin/Program.cs
+++ FileProptelin/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 
 namespace FileProptelin
 {
     class Program
     {
+        public class Options
+        {
+            [Option('f', "file", Required = true, HelpText = "ファイル指定")]
+            public string FilePath { get; set; }
+
+            [Option('d', "date", Required = true, HelpText = "対象のファイルの更新後の日付指定")]
+            public DateTime DateTime { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    var creationTime = System.IO.File.GetCreationTime(o.FilePath);
+
+                    Console.WriteLine($"対象ファイル: {o.FilePath}");
+                    Console.WriteLine($"作成日: {creationTime}");
+                    Console.WriteLine("+-------+-------+-------+-------+");
+                    System.IO.File.SetCreationTime(o.FilePath, o.DateTime);
+                    Console.WriteLine($"成功: {creationTime} -> {o.DateTime}");
+                });
         }
     }
 }
