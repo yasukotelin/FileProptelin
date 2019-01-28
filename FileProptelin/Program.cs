7df09d93608975ecbfceae732136aff502801854
yasuhiro.kato <yasuhiro.kato@cct-inc.co.jp>
2019-01-28 17:42:49 +0900

    プロジェクト ファイルを追加します。

---
+++ FileProptelin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileProptelin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
